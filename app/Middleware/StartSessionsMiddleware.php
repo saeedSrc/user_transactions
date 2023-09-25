bfb8b64c7c8bdb861949a0c493ea26763f7e7456
saeed rasooli <saeed@rasooli>
2023-09-25 21:15:24 +0330

    adding pagination

--- app/Middleware/StartSessionsMiddleware.php
+++ app/Middleware/StartSessionsMiddleware.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace App\Middleware;
 
 use App\Contracts\SessionInterface;
+use App\Services\RequestService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -12,8 +13,10 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class StartSessionsMiddleware implements MiddlewareInterface
 {
-    public function __construct(private readonly SessionInterface $session)
-    {
+    public function __construct(
+        private readonly SessionInterface $session,
+        private readonly RequestService $requestService
+    ) {
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
@@ -21,6 +24,10 @@ class StartSessionsMiddleware implements MiddlewareInterface
         $this->session->start();
 
         $response = $handler->handle($request);
+
+        if ($request->getMethod() === 'GET' && ! $this->requestService->isXhr($request)) {
+            $this->session->put('previousUrl', (string) $request->getUri());
+        }
 
         $this->session->save();
 
--- configs/routes/web.php
+++ configs/routes/web.php
@@ -3,17 +3,31 @@
 declare(strict_types = 1);
 
 use App\Controllers\AuthController;
+use App\Controllers\CategoriesController;
 use App\Controllers\HomeController;
 use App\Middleware\AuthMiddleware;
 use App\Middleware\GuestMiddleware;
 use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
     $app->get('/', [HomeController::class, 'index'])->add(AuthMiddleware::class);
 
-    $app->get('/login', [AuthController::class, 'loginView'])->add(GuestMiddleware::class);
-    $app->get('/register', [AuthController::class, 'registerView'])->add(GuestMiddleware::class);
-    $app->post('/login', [AuthController::class, 'logIn'])->add(GuestMiddleware::class);
-    $app->post('/register', [AuthController::class, 'register'])->add(GuestMiddleware::class);
+    $app->group('', function (RouteCollectorProxy $guest) {
+        $guest->get('/login', [AuthController::class, 'loginView']);
+        $guest->get('/register', [AuthController::class, 'registerView']);
+        $guest->post('/login', [AuthController::class, 'logIn']);
+        $guest->post('/register', [AuthController::class, 'register']);
+    })->add(GuestMiddleware::class);
+
     $app->post('/logout', [AuthController::class, 'logOut'])->add(AuthMiddleware::class);
+
+    $app->group('/categories', function (RouteCollectorProxy $categories) {
+        $categories->get('', [CategoriesController::class, 'index']);
+        $categories->get('/load', [CategoriesController::class, 'load']);
+        $categories->post('', [CategoriesController::class, 'store']);
+        $categories->delete('/{id:[0-9]+}', [CategoriesController::class, 'delete']);
+        $categories->get('/{id:[0-9]+}', [CategoriesController::class, 'get']);
+        $categories->post('/{id:[0-9]+}', [CategoriesController::class, 'update']);
+    })->add(AuthMiddleware::class);
 };
